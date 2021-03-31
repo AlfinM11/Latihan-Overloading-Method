41e6f14b9473b082933e55580679bd3b23e2b75e
AlfinMH <alfinmhasan07@students.amikom.ac.id>
2021-03-31 13:31:40 +0700

    Add project files.

---
+++ Latihan Overloading Method/Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Overloading_Method
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return (new int[] { number1, number2 }).Min();
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return (int)(new float[] { number1, number2, number3 }).Min();
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return (new int[] { number1, number2 }).Max();
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return (int)(new float[] { number1, number2, number3 }).Max();
+        }
+    }
+}
